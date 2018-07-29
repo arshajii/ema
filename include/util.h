e7050d6b7d9779895fed8b97b0f722fbdd084474
A. R. Shajii <ars@ars.me>
2018-07-29 16:45:02 -0400

    Allow >256 contigs in reference

--- include/util.h
+++ include/util.h
@@ -19,7 +19,7 @@
 #define IS_ACGT(c) ((c) == 'A' || (c) == 'C' || (c) == 'G' || (c) == 'T')
 
 typedef uint32_t bc_t;
-typedef uint8_t chrom_t;
+typedef uint32_t chrom_t;
 
 void copy_until_space(char *dest, char **src);
 char *escape(char *s);
